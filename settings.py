cf10d5f1f83740ff2afdee24e6cb638f206a39e9
Dmitry Golovin <golovin@quantnet.ai>
2020-02-05 03:51:56 +0300

    symbols

--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
 import os
 
 WORK_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "work")
-AVANTAGE_KEY = "key"
+SYMBOLS = ['AAPL', 'GOOGL', 'FB']
 
 MASTER_ADDR = "https://data-master.quantnet.ai"
 
-MASTER_ADDR = "http://localhost:8000"

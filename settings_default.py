3a2ad0b6d73f5d7113405a9ad0fd1cd54f44c034
Dmitry Golovin <golovin@quantnet.ai>
2020-02-07 20:32:44 +0300

    settings

--- settings_default.py
+++ settings_default.py
@@ -2,6 +2,7 @@ import os
 
 AVANTAGE_KEY = "real_key"
 WORK_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "work")
-SYMBOLS = ['AAPL', 'GOOGL', 'FB']
+SYMBOLS = ['AAPL', 'GOOG', 'FB'] # None for all
+INDEXES = ['SPX'] # None for all
 
 MASTER_ADDR = "https://data-master.quantnet.ai"

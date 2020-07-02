104e3d929c6540fe1dd0e43077468081ddb3895d
Dmitry Golovin <golovin@quantnet.ai>
2020-07-02 18:24:45 +0300

    sec.gov performance improvement

--- secgov/conf.py
+++ secgov/conf.py
@@ -2,8 +2,9 @@ import os
 from urllib.parse import urljoin
 from datarelay.settings import MASTER_ADDR, WORK_DIR
 
-SECGOV_BASE_DIR = os.path.join(WORK_DIR, 'secgov2')
+SECGOV_BASE_DIR = os.path.join(WORK_DIR, 'secgov3')
 SEC_GOV_LAST_ID_FILE_NAME = os.path.join(SECGOV_BASE_DIR, "last_id.txt")
 SECGOV_FORMS_DIR_NAME = os.path.join(SECGOV_BASE_DIR, "forms")
+SECGOV_FACTS_DIR_NAME =  os.path.join(SECGOV_BASE_DIR, "facts")
 
 BASE_URL =  urljoin(MASTER_ADDR, 'master/sec.gov/forms')
--- secgov/sync.py
+++ secgov/sync.py
@@ -1,9 +1,11 @@
+import datetime
+
 from assets.conf import ASSETS_LIST_FILE_NAME, ASSETS_DATA_DIR
 from datarelay.http import request_with_retry
 import os
 import json
 import logging
-from secgov.conf import BASE_URL, SECGOV_FORMS_DIR_NAME, SEC_GOV_LAST_ID_FILE_NAME
+from secgov.conf import BASE_URL, SECGOV_FORMS_DIR_NAME, SEC_GOV_LAST_ID_FILE_NAME, SECGOV_FACTS_DIR_NAME
 from datarelay.settings import SECGOV_INCREMENTAL_UPDATE
 import gzip
 import zipfile
@@ -49,28 +51,63 @@ def sync():
         for r in lst:
             last_id = max(last_id, r['id'])
 
+            r['id'] = r['url'].split('/data/', 1)[1].split('/',2)[1]
+
+            forms_dir = os.path.join(SECGOV_FORMS_DIR_NAME, r['cik'])
+            os.makedirs(forms_dir, exist_ok=True)
+
+            report_fn = r['date'] + '$' + r['type'].replace('/', '-') + '$' + r['id'] + ".json.gz"
+            report_fn = os.path.join(forms_dir, report_fn)
+            with gzip.open(report_fn,'wt') as f:
+                f.write(json.dumps(r, indent=1))
+
+            facts_dir = os.path.join(SECGOV_FACTS_DIR_NAME, r['cik'])
+            os.makedirs(facts_dir, exist_ok=True)
+
             facts = r['facts']
-            del r['facts']
+            facts = [{
+                'cik':r['cik'],
+                'report_id':r['id'],
+                'report_date':r['date'],
+                'report_type':r['type'],
+                'report_url':r['url'],
 
-            os.makedirs(os.path.join(SECGOV_FORMS_DIR_NAME, r['cik']), exist_ok=True)
+                'fact_name': f['name'],
+                'segment': f['segment'],
+                'value': f['value'],
 
-            container_fn = r['date'] + '$' + r['type'].replace('/', '-') + '$' + r['url'].split('/data/', 1)[1].split('/',2)[1] + ".zip"
-            container_fn = os.path.join(SECGOV_FORMS_DIR_NAME, r['cik'], container_fn)
+                'period': f['period']['value'],
+                'period_type': f['period']['type'],
+                'period_length': get_period_length(f),
 
-            with zipfile.ZipFile(container_fn, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
-                r = json.dumps(r, indent=1)
-                z.writestr('meta.json', r)
-                facts = list(sorted(facts, key=lambda f: f['name']))
-                facts = [(g[0], list(g[1])) for g in groupby(facts, lambda f: f['name'])]
-                for g in facts:
-                    fn = "facts/" +  g[0] + '.json'
-                    ff = list(g[1])
-                    ff = json.dumps(ff, indent=1)
-                    z.writestr(fn, ff)
+                'unit': f['unit']['value'] if f['unit'] is not None else None,
+                'unit_type': f['unit']['type'] if f['unit'] is not None else None,
+            } for f in facts]
+
+            facts = list(sorted(facts, key=lambda f: f['fact_name']))
+            facts = [(g[0], list(g[1])) for g in groupby(facts, lambda f: f['fact_name'])]
+
+            for (n, ff) in facts:
+                fn = os.path.join(facts_dir, n + ".json.list.gz")
+                with gzip.open(fn, 'at') as f:
+                    for fact in ff:
+                        f.write(json.dumps(fact) + "\n")
 
         with open(SEC_GOV_LAST_ID_FILE_NAME, 'w') as f:
             f.write(str(last_id))
     logger.info("Done.")
+
+
+def get_period_length(fact):
+    if type(fact['period']['value']) == list:
+        start = datetime.datetime.strptime(fact['period']['value'][0], DATE_FORMAT).date()
+        end = datetime.datetime.strptime(fact['period']['value'][1], DATE_FORMAT).date()
+        return (end - start).days
+    else:
+        return None
+
+
+DATE_FORMAT = '%Y-%m-%d'
 
 
 if __name__ == "__main__":
